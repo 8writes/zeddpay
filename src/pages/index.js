255fac766933791b530f4bf77eba06837c431cb8
Emmanuel Chisom <ozimmanuel79@gmail.com>
2022-04-26 11:09:48 +0100

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,91 +41,89 @@ const IndexPage = () => (
             <div class="m-5 mb-4 md:mb-5">
               <div className="flex flex-nowrap md:flex-wrap">
                 <div class=" mt-5 md:mt-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="white"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                />
-              </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-10 w-10"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                    />
+                  </svg>
+                </div>
+                <div class="mt-3 md:mt-0">
+                  <p class="text-base leading-relaxed text-white text-center">
+                    <a className="text-blue-700">Zeddpay</a> converts your
+                    airtime to zeddcoin within minutes.
+                  </p>
+                </div>
               </div>
-              <div class="mt-3 md:mt-0">
-              <p class="text-base leading-relaxed text-white text-center">
-                <a className="text-blue-700">Zeddpay</a> converts your airtime
-                to zeddcoin within minutes.
-              </p>
             </div>
           </div>
-</div>
-</div>
           <div class="bg-slate-800 rounded-lg overflow-hidden hover:scale-105 ease-out duration-200 font-righteous">
-           <div class="m-5 mb-4 md:mb-5">
-              <div className="flex flex-nowrap md:flex-wrap">
-                <div class=" mt-5 md:mt-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="white"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              </div>
-              <div class="mt-4 md:mt-0">
-              <p class="text-base leading-relaxed text-white text-center">
-                <a className="text-blue-800">Zeddpay</a> enables users easily
-                make purchases with airtime.
-              </p>
-            </div>
-          </div>
-</div>
-</div>
-          <div class="bg-slate-800 rounded-lg overflow-hidden hover:scale-105 ease-out duration-200 font-righteous">
-
             <div class="m-5 mb-4 md:mb-5">
               <div className="flex flex-nowrap md:flex-wrap">
                 <div class=" mt-5 md:mt-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="white"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                />
-              </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-10 w-10"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                </div>
+                <div class="mt-4 md:mt-0">
+                  <p class="text-base leading-relaxed text-white text-center">
+                    <a className="text-blue-800">Zeddpay</a> enables users
+                    easily make purchases with airtime.
+                  </p>
+                </div>
               </div>
+            </div>
+          </div>
+          <div class="bg-slate-800 rounded-lg overflow-hidden hover:scale-105 ease-out duration-200 font-righteous">
+            <div class="m-5 mb-4 md:mb-5">
+              <div className="flex flex-nowrap md:flex-wrap">
+                <div class=" mt-5 md:mt-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-10 w-10"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                    />
+                  </svg>
+                </div>
 
-              <div class="mt-3 md:mt-0">
-              <p class="text-base leading-relaxed text-white text-center">
-                <a className="text-blue-800">Zeddpay</a> offers a secure and
-                safe transaction process.
-              </p>
+                <div class="mt-3 md:mt-0">
+                  <p class="text-base leading-relaxed text-white text-center">
+                    <a className="text-blue-800">Zeddpay</a> offers a secure and
+                    safe transaction process.
+                  </p>
+                </div>
               </div>
-
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
     <script
       dangerouslySetInnerHTML={{
